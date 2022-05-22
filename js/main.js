272f7034b8c34e520e32316c7de3aa4bcf74a1d9
Sergey Goncharovр <doimpos@gmail.com>
2022-05-22 20:56:50 +0500

    block done

--- js/main.js
+++ js/main.js
@@ -31,4 +31,69 @@ $(function() {
           }
         ]
     });
+    $('.sl_reviews').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: false,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 581,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              variableWidth: true,
+              dots: false
+            }
+          }
+        ]
+    });
+    $('.sl_prods').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: false,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 781,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 481,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              variableWidth: true,
+              dots: false
+            }
+          }
+        ]
+    });
+    $('.main_reviews__more').on('click', function() {
+        $(this).parents('.main_reviews__block').find('.main_reviews__hide').slideToggle();
+        $(this).toggleClass('isActive');
+        if ($(this).hasClass('isActive')) {
+            $(this).text('Скрыть');
+        } else {
+            $(this).text('Читать полностью');
+        }
+    })
 })
